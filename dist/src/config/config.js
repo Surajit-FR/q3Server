e7f8f8dc9760294be04c14f10ac82c4412a032fc
simran3011-node <simran.agarwal@ariprodesigns.com>
2025-08-07 16:35:37 +0530

    dist removed

--- dist/src/config/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TWILIO_PHONE_NUMBER = exports.TWILIO_AUTH_TOKEN = exports.TWILIO_ACCOUNT_SID = void 0;
-exports.TWILIO_ACCOUNT_SID = "";
-exports.TWILIO_AUTH_TOKEN = "";
-exports.TWILIO_PHONE_NUMBER = "";
