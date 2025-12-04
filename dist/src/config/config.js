3d3b7c4e6afe8fd10f4bd73b7f389973a7c9eed1
Surajit-FR <surojit.pal@ariprodesigns.com>
2025-12-04 19:29:02 +0530

    removed dist

--- dist/src/config/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.STRIPE_WEBHOOK_SECRET = exports.STRIPE_SECRET_KEY = exports.TWILIO_PHONE_NUMBER = exports.TWILIO_AUTH_TOKEN = exports.TWILIO_ACCOUNT_SID = void 0;
-exports.TWILIO_ACCOUNT_SID = "";
-exports.TWILIO_AUTH_TOKEN = "";
-exports.TWILIO_PHONE_NUMBER = "";
-exports.STRIPE_SECRET_KEY = "";
-exports.STRIPE_WEBHOOK_SECRET = "";
